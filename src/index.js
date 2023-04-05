780a329a6d102e0303b840ca7cafad4dda0fdd15
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-04-05 16:23:56 +0300

    DOne

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // code to run when the DOM is loaded
+  const form = document.getElementById("todo-form");
+  const input = document.getElementById("todo-input");
+  const list = document.getElementById("todo-list");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent default form submission behavior
+    const task = input.value; // get the task from the input field
+    if (task.trim() !== "") { // make sure the task is not empty or only whitespace
+      const listItem = document.createElement("li");
+      listItem.textContent = task;
+      list.appendChild(listItem);
+      input.value = "";
+    }
+  });
 });
